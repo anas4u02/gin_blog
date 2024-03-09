1ce2cf103779f9c724bf8991a500a04e31983169
Mohammad Anas <anas4u02@gmail.com>
2024-03-09 17:06:30 +0530

    Initialized the server with using data from config file

--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"blog/config"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"net/http"
 )
 
 func main() {
+	configs := configSet()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message":  "pong",
+			"app name": viper.Get("App.Name"),
 		})
 	})
-	err := r.Run()
+	err := r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
 	if err != nil {
 		return
-	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	}
+}
+
+func configSet() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading the config!")
+	}
+
+	var configs config.Config
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+
+	return configs
 }
