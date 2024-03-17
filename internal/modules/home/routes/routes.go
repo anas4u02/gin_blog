a1c84d3d76c34604f083560bf03f97181a6af2d7
Mohammad Anas <anas4u02@gmail.com>
2024-03-17 16:42:36 +0530

    Added HTML templates as well as routes for the same

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,16 +1,21 @@
 package routes
 
 import (
+	"blog/pkg/html"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 	"net/http"
 )
 
 func Routes(router *gin.Engine) {
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":  "pong",
-			"app name": viper.Get("App.Name"),
+		html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
+			"title": "Home Page",
+		})
+	})
+
+	router.GET("/about", func(c *gin.Context) {
+		html.Render(c, http.StatusOK, "modules/home/html/about", gin.H{
+			"title": "About Page",
 		})
 	})
 }
--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -2,13 +2,19 @@ package bootstrap
 
 import (
 	"blog/pkg/config"
+	"blog/pkg/html"
 	"blog/pkg/routing"
+	"blog/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 
 	routing.Init()
+
+	static.LoadStatic(routing.GetRouter())
+
+	html.LoadHTML(routing.GetRouter())
 
 	routing.RegisterRoutes()
 
